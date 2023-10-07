73ebe0d8b936d5aa2f76860a8396b1bae66391d4
Avni <2025agarg@tjhsst.edu>
2023-10-07 19:57:44 -0400

    testing views

--- App.js
+++ App.js
@@ -1,25 +1,41 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './pages/Login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './pages/Login';
 import Home from './pages/Home';
-import House from  './pages/House';
+import House from './pages/House';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import HousePage from './pages/HousePage';
 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="House" component={House} />
 
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="House" component={House} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+      {/* <View style={{ flex: 1, backgroundColor: 'lightblue' }}>
+        <Header />
+        <HousePage />
+      </View>
+
+      <View style={{ backgroundColor: 'lightblue' }}>
+        <Footer />
+      </View> */}
+
+    </>
+
   );
 }
 
@@ -27,7 +43,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
