6a15225366fcd2a4e664f8491078ac85be772f60
Anmol Karan <anmolkaran@Anmols-MacBook-Pro.local>
2023-10-07 13:35:07 -0400

    added nav

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './pages/Login';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-        <Login />
-      </>
-    </View>
+    
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
