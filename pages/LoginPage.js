f0416401410f60491b4a770b41e72710c63629a3
Avni <2025agarg@tjhsst.edu>
2023-10-08 13:08:28 -0400

    added login page

---
+++ pages/LoginPage.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableWithoutFeedback,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    Platform,
+    Keyboard,
+    StyleSheet,
+} from 'react-native';
+import Header from '../components/Header';
+
+
+const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={loginStyles.container}>
+                <Header />
+
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === "ios" ? "padding" : "height"}
+                    style={loginStyles.inner}
+                >
+                    <Text style={loginStyles.loginText}>
+                        Login
+                    </Text>
+                    <TextInput
+                        style={loginStyles.input}
+                        placeholder='Username'
+                        onChangeText={setUsername}
+                        value={username}
+                    />
+                    <TextInput
+                        style={loginStyles.input}
+                        placeholder='Password'
+                        onChangeText={setPassword}
+                        value={password}
+                        secureTextEntry={true}
+                    />
+
+                </KeyboardAvoidingView>
+            </View>
+        </TouchableWithoutFeedback>
+    );
+}
+
+const loginStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'lightblue',
+    },
+    inner: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    footerContainer: {
+        backgroundColor: 'lightblue'
+    },
+    loginText: {
+        fontSize: 30,
+        color: 'black',
+        textAlign: 'center',
+        paddingBottom: 20
+    },
+    input: {
+        width: 200,
+        height: 44,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 10,
+    },
+});
+
+export default LoginPage;
