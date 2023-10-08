7375b6782eb1a52cdcfbcc165c94fe2d00e7a058
Avni <2025agarg@tjhsst.edu>
2023-10-08 11:13:14 -0400

    added text input

--- pages/HousePage.js
+++ pages/HousePage.js
@@ -1,26 +1,76 @@
-import { StyleSheet, View, Text, ScrollView } from 'react-native';
-
-const properties = [
-    'Number of People in Household',
-    'Zip Code',
-    'House Size',
-    'Type of Housing',
-    'Energy Source',
-    'Energy consupmtion per month',
-    'Primary form of transportation',
-
-]
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, ScrollView, TextInput } from 'react-native';
 
 const Inputs = () => {
+    const [people, setPeople] = useState('');
+    const [zip, setZip] = useState('');
+    const [size, setSize] = useState('');
+    const [type, setType] = useState('');
+    const [source, setSource] = useState('');
+    const [consumption, setConsumption] = useState('');
+    const [transportation, setTransportation] = useState('');
+
+    const questions = [
+        {
+            text: 'Number of People in Household',
+            id: 'people',
+            state: people,
+            onChangeState: setPeople,
+            type: 'number-pad',
+        },
+        {
+            text: 'Zip Code',
+            id: 'zip',
+            state: zip,
+            onChangeState: setZip,
+            type: 'number-pad',
+            length: 5,
+        },
+        {
+            text: 'House Size (in square feet)',
+            id: 'size', state: size,
+            onChangeState: setSize,
+            type: 'decimal-pad',
+        },
+        {
+            text: 'Type of Housing',
+            id: 'type', state: type,
+            onChangeState: setType
+        },
+        {
+            text: 'Energy Source',
+            id: 'source', state: source,
+            onChangeState: setSource
+        },
+        {
+            text: 'Energy consupmtion per month',
+            id: 'consumption', state: consumption,
+            onChangeState: setConsumption,
+            type: 'decimal-pad',
+        },
+        {
+            text: 'Primary form of transportation',
+            id: 'transportation', state: transportation, 
+            onChangeState: setTransportation
+        },
+    ]
+
     return (
         <ScrollView indicatorStyle={"navy"} style={houseStyles.scrollContainer}>
-            <Text style={houseStyles.itemText}>
-                {properties.map((property) => (
-                    <Text key={property}>
-                        {property + '\n'}
+            {questions.map((question) => (
+                <>
+                    <Text key={question.id} style={houseStyles.itemText}>
+                        {question.text + '\n'}
                     </Text>
-                ))}
-            </Text>
+                    <TextInput
+                        value={question.state}
+                        onChangeText={question.onChangeState}
+                        keyboardType={question.type}
+                        maxLength={question.length}
+                        style={houseStyles.input}
+                    />
+                </>
+            ))}
         </ScrollView>
     )
 }
@@ -38,21 +88,31 @@ const HousePage = () => {
 
 const houseStyles = StyleSheet.create({
     container: {
-        flex: 0.8
+        flex: 0.85
     },
     scrollContainer: {
         padding: 20,
         backgroundColor: 'lightcyan'
     },
     titleText: {
-        padding: 20, 
-        fontSize: 30, 
-        flexWrap: 'wrap', 
+        padding: 20,
+        fontSize: 30,
+        flexWrap: 'wrap',
         textAlign: 'center'
     },
     itemText: {
-        fontSize: 20
+        fontSize: 20,
+        margin: 0,
+    },
+    input: {
+        height: 40,
+        marginBottom: 20,
+        borderWidth: 1,
+        padding: 10,
+        fontSize: 16,
+        borderColor: 'black',
+        backgroundColor: 'azure',
     }
-  });
+});
 
 export default HousePage;
