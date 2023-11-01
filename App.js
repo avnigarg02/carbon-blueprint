8a6810ffefac5cf9489f973f155ac0b7ffbad0b1
Avni <2025agarg@tjhsst.edu>
2023-11-01 14:33:51 -0400

    added nav bar titles

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import ModifyHousePage from './pages/ModifyHousePage.js';
 import LoginPage from'./pages/LoginPage';
@@ -27,34 +25,25 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="Login" component={LoginPage}  /> 
           <Stack.Screen name="Home" component={HomePage} 
-          options={{ title: 'Home' ,headerLeft: () => (
+          options={{ title: 'Home', headerLeft: () => (
             <Button
               onPress={() => alert('Info button test')}
               title="Info"
             />
           ),}}/>
-          <Stack.Screen name="ModifyHouse" component={ModifyHousePage} options = {{title:"House Settings"}} />
-          <Stack.Screen name="Spending" component={SpendingPage}  /> 
-          <Stack.Screen name="House" component={HousePage} />
-          <Stack.Screen name="Thermostat" component={ThermostatPage} />
-          <Stack.Screen name="Signup" component={SignupPage} />
-          <Stack.Screen name="HomeEnergy" component={HomeEnergyPage} />
-          <Stack.Screen name="HouseholdVehicles" component={householdVehiclesPage} />
-          <Stack.Screen name="Waste" component={wastePage} />
-          <Stack.Screen name="GeneralInfo" component={GeneralInfoPage} />
+          <Stack.Screen name="ModifyHouse" component={ModifyHousePage} options={{title:"House Settings"}} />
+          <Stack.Screen name="Spending" component={SpendingPage} options={{title:"Spending"}} /> 
+          <Stack.Screen name="House" component={HousePage} options={{title:"House"}} />
+          <Stack.Screen name="Thermostat" component={ThermostatPage} options={{title:"Thermostat"}} />
+          <Stack.Screen name="Signup" component={SignupPage} options={{title:"Signup"}} />
+          <Stack.Screen name="HomeEnergy" component={HomeEnergyPage} options={{title:"Home Energy"}} />
+          <Stack.Screen name="HouseholdVehicles" component={householdVehiclesPage} options={{title:"Household Vehicles"}} />
+          <Stack.Screen name="Waste" component={wastePage} options={{title:"Waste"}} />
+          <Stack.Screen name="GeneralInfo" component={GeneralInfoPage} options={{title:"General Info"}} />
 
 
         </Stack.Navigator>
       </NavigationContainer>
-
-      {/* <View style={styles.container}>
-        <Header />
-        <HousePage />
-      </View>
-
-      <View style={styles.footerContainer}>
-        <Footer />
-      </View> */}
 
     </>
 
